631c7a45b5adc4d0abfbaec4e073fc705a873782
stas <stas.podolsky@gmail.com>
2013-06-17 21:04:58 +0300

    new file

---
+++ src/RayTracing/Ellipse.java
@@ -0,0 +1,10 @@
+package RayTracing;
+
+public class Ellipse extends Sphere {
+
+	public Ellipse(Vector center, double radius, Material mat, int matIndex) {
+		super(center, radius, mat, matIndex);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
